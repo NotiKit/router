53132b7a060d7b1432179e2dd1961e762e2f21e2
NotiKit <130459795+NotiKit@users.noreply.github.com>
2025-01-14 22:56:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -133,6 +133,23 @@ app.post('/time', async (req, res) => {
     }
 });
 
+app.post('/info', async (req, res) => {
+  const { token } = req.body;
+  try {
+      const response = await axios.post(`https://${clouds[Math.floor(Math.random() * clouds.length)]}/info`,
+          `token=${token}`,
+          {
+              headers: {
+                  "content-type": "application/x-www-form-urlencoded"
+              }
+          }
+      );
+      res.json(response.data);
+  } catch (error) {
+      res.status(error.response && error.response.status || 500).json({ error: error.message });
+  }
+});
+
 // 10. POST Request to /gift
 app.post('/gift', async (req, res) => {
     const { token } = req.body;
