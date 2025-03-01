88b4637c6b8477b2a4ab71debe7d695b4befa7ce
NotiKit <130459795+NotiKit@users.noreply.github.com>
2025-03-01 12:19:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const https = require('https');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-/*
+
 const clouds = [
     "flare.cloud1-917.workers.dev",
     "flare.cloud2-d98.workers.dev",
@@ -12,7 +12,7 @@ const clouds = [
     "flare.cloud4-8ec.workers.dev",
     "flare.cloud5-2cd.workers.dev"
 ];
-*/
+/*
 const clouds = [
     "flare.cloud1-917.workers.dev",
     "flare.cloud2-d98.workers.dev",
@@ -24,6 +24,7 @@ const clouds = [
     "flare.cloud5-2cd.workers.dev",
     "flare.cloud1-917.workers.dev"
 ];
+*/
 
 
 function keepRunning() {
